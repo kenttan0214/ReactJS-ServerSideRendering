698d3ce525653d6561e95c4c50697108a8d8440a
SEEKASIA\kenttan <kenttan@seekasia.com>
2017-03-20 17:06:41 +0800

    add basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+
+module.exports = function () {
+
+    var webpackConfig = {
+        context: path.resolve('app'),
+        entry: {
+            app: './app'
+        },
+        output: {
+            path: path.resolve('build'),
+            filename: '[hash][id].js',
+            chunkFilename: '[name][chunkhash][id].js'
+        },
+        devtool: '#eval',
+        module: {
+            rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                  presets:[ 'es2015', 'react', 'stage-2']
+                }
+            }]
+        },
+        plugins: [],
+        resolve: {
+            modules: ['node_modules'],
+            extensions: ['.js', 'jsx'],
+            alias: {
+            }
+        },
+        resolveLoader: {
+        }
+    };
+    return webpackConfig;
+}
